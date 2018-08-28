f9d5afb1589481a62d45dc5e306d9ad204ad4c82
jaerentredway <jwilliamtredway@gmail.com>
2018-08-28 14:58:09 -0600

    First commit with HelloWorld.java file

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * @author will
+ * Will's first practice Java class.
+ */
+public class HelloWorld {
+
+  /**
+   * @param args: method arguments are not used by this method.
+   * This main method prints the phrase "Hello World!"
+   */ 
+  public static void main(String[] args) {
+   System.out.println("Hello World!");
+
+  }
+
+}
